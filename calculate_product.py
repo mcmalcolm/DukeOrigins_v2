c8e23ab2c406d6520eea146c19159469ba64da6c
mcmalcolm <81266408+mcmalcolm@users.noreply.github.com>
2021-06-16 23:29:30 -0400

    commit with python files 1

---
+++ calculate_product.py
@@ -0,0 +1,4 @@
+x = int(input('Number1: '))
+y = int(input('Number2: '))
+product = x*y
+print(product)
---
+++ calculate_sums.py
@@ -0,0 +1,4 @@
+x = int(input('Number1: '))
+y = int(input('Number2: '))
+sum = x+y
+print(sum)
